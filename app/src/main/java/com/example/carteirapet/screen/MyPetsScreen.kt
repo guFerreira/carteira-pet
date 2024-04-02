a860ce06d755ea09231d114097e545d0e1c8abd0
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2024-04-01 23:48:49 -0300

    atualizando gitignore

---
+++ app/src/main/java/com/example/carteirapet/screen/MyPetsScreen.kt
@@ -0,0 +1,148 @@
+package com.example.carteirapet.screen
+
+import android.content.res.Resources.Theme
+import android.graphics.drawable.shapes.Shape
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.carteirapet.R
+import com.example.carteirapet.ui.theme.CarteiraPetTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyPetsScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onSecondary
+                ),
+                title = { Text("Meus Pets") },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                })
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary,
+                shape = RoundedCornerShape(100.dp),
+                onClick = {  },
+                icon = { Icon(Icons.Filled.Add, "Extended floating action button.", ) },
+                text = { Text(text = "Registrar Novo Pet") },
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = "Visualize as vacinas selecionando um de seus pets",
+            )
+
+
+            LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
+                items(100) { i ->
+
+                    CardPet()
+
+
+                }
+            })
+        }
+    }
+}
+
+@Composable
+@Preview
+fun MyPetsScreenPreview() {
+    CarteiraPetTheme {
+        MyPetsScreen()
+    }
+}
+
+
+@Composable
+fun CardPet() {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.padding(8.dp)) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .background(
+                    color = MaterialTheme.colorScheme.primaryContainer,
+                    shape = RoundedCornerShape(18.dp)
+                )
+                .padding(12.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.bolinha),
+                contentDescription = "Imagem do pet",
+                Modifier
+                    .border(1.dp, MaterialTheme.colorScheme.onPrimaryContainer, CircleShape)
+                    .size(120.dp)
+            )
+            Text(text = "Bolinha")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun CardPreview() {
+    CarteiraPetTheme {
+        CardPet()
+    }
+}
