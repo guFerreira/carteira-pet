988c615cff21575399eefcf7187a991948ad2804
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2024-04-24 21:21:25 -0300

    criando modal de detalhes das vacinas

--- app/src/main/java/com/example/carteirapet/screen/PetInformationScreen.kt
+++ app/src/main/java/com/example/carteirapet/screen/PetInformationScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,18 +29,26 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -49,6 +58,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.carteirapet.R
 import com.example.carteirapet.ui.theme.CarteiraPetTheme
+import kotlinx.coroutines.launch
 
 class PetInformationScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -176,12 +186,19 @@ fun Vaccines() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VaccineItem() {
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
     Column(
         Modifier
             .fillMaxWidth()
             .padding(8.dp)
+            .clickable {
+                showBottomSheet = true
+            }
     ) {
         Text(text = "10/10/2024")
         Text(text = "Vacina contra raiva")
@@ -189,6 +206,34 @@ fun VaccineItem() {
         Text(text = "12a636fcab5953233706dadacfff3ba8")
         Spacer(modifier = Modifier.height(2.dp))
         Divider(modifier = Modifier.height(1.dp))
+
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    showBottomSheet = false
+                },
+                sheetState = sheetState,
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+                modifier = Modifier.height(420.dp)
+            ) {
+                Column {
+                    Text(text = "Vacina contra Raiva")
+                    Text(text = "Data da aplicação: 10/10/2023/ às 14:54")
+                    Text(text = "Lote: 123456789")
+                    Text(text = "Fabricante: Pfizer")
+                    Text(text = "Médico veterinário: João da Silva Teste")
+                    Text(text = "CRMV: 12345")
+
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Divider(modifier = Modifier.height(1.dp))
+                    Text(text = "Assinado Digitalmente")
+                    Text(text = "12a636fcab5953233706dadacfff3ba8")
+                }
+
+            }
+        }
     }
 }
 
